a7fad519a6c54946c167a7148b47f788083a447f
luke0609 <ljs19950609@163.com>
2016-10-13 19:16:15 +0800

    author:徐小龙 desc:时间10/13  添加类httputils

---
+++ app/src/main/java/linchom/com/linchomspace/shopping/contant/HttpUtils.java
@@ -0,0 +1,17 @@
+package linchom.com.linchomspace.shopping.contant;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+
+public class HttpUtils {
+
+    public static  final String GOODSLISTURL="http://app.linchom.com/appProduct.php?type=goodslist&act=list";
+
+    public static  final String KEY="linchom";
+
+    public static  final String VERIFICATION="e0d017ef76c8510244ebe0191f5dde15";
+
+
+
+}
