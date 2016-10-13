c3fe6ddb78c9b71ae0d38525383efbd49a4095ab
luke0609 <ljs19950609@163.com>
2016-10-13 20:46:05 +0800

    author:徐小龙 desc:时间10/13  添加工具类

---
+++ app/src/main/java/linchom/com/linchomspace/shopping/utils/GoodsXUtilsImage.java
@@ -0,0 +1,67 @@
+package linchom.com.linchomspace.shopping.utils;
+
+import android.widget.ImageView;
+
+import org.xutils.common.util.DensityUtil;
+import org.xutils.image.ImageOptions;
+import org.xutils.x;
+
+import linchom.com.linchomspace.R;
+
+/**
+ * Created by Administrator on 2016/9/26.
+ */
+public class GoodsXUtilsImage {
+
+
+
+
+    public static void display(ImageView imageView, String iconUrl) {
+        ImageOptions imageOptions = new ImageOptions.Builder()
+                .setIgnoreGif(false)//是否忽略gif图。false表示不忽略。不写这句，默认是true
+                .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                .setFailureDrawableId(R.mipmap.ic_launcher)
+                .setLoadingDrawableId(R.mipmap.ic_launcher)
+                .build();
+        x.image().bind(imageView, iconUrl,imageOptions);
+    }
+
+
+
+    public static void display(ImageView imageView, String iconUrl, int radius) {
+        ImageOptions imageOptions = new ImageOptions.Builder()
+                .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                .setRadius(DensityUtil.dip2px(radius))
+                .setIgnoreGif(false)
+                .setCrop(false)//是否对图片进行裁剪
+                .setFailureDrawableId(R.mipmap.ic_launcher)
+                .setLoadingDrawableId(R.mipmap.ic_launcher)
+                .build();
+        x.image().bind(imageView, iconUrl, imageOptions);
+    }
+
+    /**
+     * 显示圆形头像，第三个参数为true
+     *
+     * @param imageView  图像控件
+     * @param iconUrl    图片地址
+     * @param isCircluar 是否显示圆形
+     */
+    public static void display(ImageView imageView, String iconUrl, boolean isCircluar) {
+        ImageOptions imageOptions = new ImageOptions.Builder()
+                .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                .setCircular(isCircluar)
+                .setCrop(true)
+                .setLoadingDrawableId(R.mipmap.ic_launcher)
+                .setFailureDrawableId(R.mipmap.ic_launcher)
+                .build();
+        x.image().bind(imageView, iconUrl, imageOptions);
+    }
+
+
+
+
+
+
+
+}
