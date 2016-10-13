d9b4ac0481980fcb5be432ea0d68e8239382c332
luke0609 <ljs19950609@163.com>
2016-10-13 14:54:44 +0800

    author:徐小龙 desc:上传测试10/13

---
+++ app/src/main/java/linchom/com/linchomspace/shopping/goodstest/ImagesFromNet.java
@@ -0,0 +1,16 @@
+package linchom.com.linchomspace.shopping.goodstest;
+
+/**
+ * Created by Administrator on 2016/9/28.
+ */
+public class ImagesFromNet {
+
+
+    public String picture1;
+    public String picture2;
+    public String picture3;
+    public String picture4;
+
+
+
+}
