0c8fc56637cb1f07af41c3fea2b339f223386da9
Administrator <ljs19950609@163.com>
2016-10-14 09:10:29 +0800

    LcL

---
+++ app/src/main/java/linchom/com/linchomspace/mine/Details_s_Activity.java
@@ -0,0 +1,15 @@
+package linchom.com.linchomspace.mine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import linchom.com.linchomspace.R;
+
+public class Details_s_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_s_);
+    }
+}
