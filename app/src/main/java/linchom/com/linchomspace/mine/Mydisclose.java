ff00defc614a35facb5ac094ffeca0b89f8857fb
lenovo <ljs19950609@163.com>
2016-10-19 19:46:28 +0800

    author:lcl desc:10/19

--- app/src/main/java/linchom/com/linchomspace/mine/Mydisclose.java
+++ app/src/main/java/linchom/com/linchomspace/mine/Mydisclose.java
@@ -10,6 +10,6 @@ public class Mydisclose extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mydisclose2);
+        setContentView(R.layout.activity_mydisclose);
     }
 }
