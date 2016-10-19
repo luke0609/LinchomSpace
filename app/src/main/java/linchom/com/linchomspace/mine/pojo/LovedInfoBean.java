6f56464d14fd3db413ba3b0db5333d978ca1a246
lenovo <ljs19950609@163.com>
2016-10-19 20:40:24 +0800

    author:lcl desc:10/19

---
+++ app/src/main/java/linchom/com/linchomspace/mine/pojo/LovedInfoBean.java
@@ -0,0 +1,10 @@
+package linchom.com.linchomspace.mine.pojo;
+
+/**
+ * Created by lenovo on 2016/10/19.
+ */
+public class LovedInfoBean {
+    /**
+     * 	{"result":"0","data":{"total_pages":25958,"total":"259580","items":{"":{"rec_id":"259609","is_attention":"0","goods_id":null,"goods_name":null,"market_price":"\uffe50\u5143","shop_price":"\uffe50\u5143","promote_price":"","url":false}},"pagesize":10},"msg":"","url":""}
+     */
+}
