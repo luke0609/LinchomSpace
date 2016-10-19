707a133c415d507f1d9a73cb0a756a524acc1a1f
lenovo <ljs19950609@163.com>
2016-10-19 18:59:40 +0800

    author:lcl desc:10/19

--- app/src/main/java/linchom/com/linchomspace/mine/Disclose_activity.java
+++ app/src/main/java/linchom/com/linchomspace/mine/Disclose_activity.java
@@ -1,10 +1,12 @@
 package linchom.com.linchomspace.mine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import linchom.com.linchomspace.R;
@@ -13,13 +15,22 @@ public class Disclose_activity extends AppCompatActivity {
 
     private ImageView iv_disback;
     private Button b_submit;
+    private TextView tv_look;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disclose_activity);
 
+        tv_look = ((TextView)findViewById(R.id.tv_look));
         iv_disback = ((ImageView) findViewById(R.id.iv_disback));
-
+        tv_look.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Mydisclose.class);
+                startActivity(intent);
+            }
+        });
         iv_disback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
