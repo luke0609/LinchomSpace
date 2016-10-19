9a0b4e34f1bc8e43f4ba6c98ca17c599bc2f1d1b
lenovo <ljs19950609@163.com>
2016-10-19 19:37:29 +0800

    author:lcl desc:10/19

--- app/src/main/java/linchom/com/linchomspace/mine/Mydisclose.java
+++ app/src/main/java/linchom/com/linchomspace/mine/Mydisclose.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import linchom.com.linchomspace.R;
 
-public class Night_activity extends AppCompatActivity {
+public class Mydisclose extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_night_activity);
+        setContentView(R.layout.activity_mydisclose2);
     }
 }
