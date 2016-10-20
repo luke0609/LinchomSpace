6be8ab4ec04d57929a2392086008692e0be5b3d8
luke0609 <ljs19950609@163.com>
2016-10-20 18:29:11 +0800

    author:徐小龙 desc:修改

--- app/src/main/java/linchom/com/linchomspace/shopping/pojo/GoodsOrderBean.java
+++ app/src/main/java/linchom/com/linchomspace/shopping/pojo/GoodsOrderBean.java
@@ -5,4 +5,19 @@ package linchom.com.linchomspace.shopping.pojo;
  */
 
 public class GoodsOrderBean {
+
+    public String goodsNum;
+    public String goodsImg;
+    public String goodsName;
+    public String goodsPrice;
+
+    public GoodsOrderBean(String goodsNum, String goodsImg, String goodsName, String goodsPrice) {
+        this.goodsNum = goodsNum;
+        this.goodsImg = goodsImg;
+        this.goodsName = goodsName;
+        this.goodsPrice = goodsPrice;
+    }
+
+
+
 }
