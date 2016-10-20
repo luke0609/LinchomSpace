d415ea6be3ae67e49a3e64f11c5f88843af4859e
luke0609 <ljs19950609@163.com>
2016-10-20 18:48:11 +0800

    author:徐小龙 desc:修改

--- app/src/main/java/linchom/com/linchomspace/shopping/pojo/GoodsOrderBean.java
+++ app/src/main/java/linchom/com/linchomspace/shopping/pojo/GoodsOrderBean.java
@@ -1,13 +1,12 @@
 package linchom.com.linchomspace.shopping.pojo;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.io.Serializable;
 
 /**
  * Created by Administrator on 2016/10/20.
  */
 
-public class GoodsOrderBean implements Parcelable {
+public class GoodsOrderBean implements Serializable {
 
     public String goodsNum;
     public String goodsImg;
@@ -22,35 +21,5 @@ public class GoodsOrderBean implements Parcelable {
     }
 
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.goodsNum);
-        dest.writeString(this.goodsImg);
-        dest.writeString(this.goodsName);
-        dest.writeString(this.goodsPrice);
-    }
-
-    protected GoodsOrderBean(Parcel in) {
-        this.goodsNum = in.readString();
-        this.goodsImg = in.readString();
-        this.goodsName = in.readString();
-        this.goodsPrice = in.readString();
-    }
-
-    public static final Parcelable.Creator<GoodsOrderBean> CREATOR = new Parcelable.Creator<GoodsOrderBean>() {
-        @Override
-        public GoodsOrderBean createFromParcel(Parcel source) {
-            return new GoodsOrderBean(source);
-        }
-
-        @Override
-        public GoodsOrderBean[] newArray(int size) {
-            return new GoodsOrderBean[size];
-        }
-    };
 }
