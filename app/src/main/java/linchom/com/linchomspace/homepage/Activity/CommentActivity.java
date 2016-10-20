795bea126a5caee5cf8dc1a101de8b77f9362e3c
fc <943051230@qq.com>
2016-10-20 13:46:34 +0800

    author:fc desc:文章界面修改

--- app/src/main/java/linchom/com/linchomspace/homepage/Activity/CommentActivity.java
+++ app/src/main/java/linchom/com/linchomspace/homepage/Activity/CommentActivity.java
@@ -1,15 +1,15 @@
-package linchom.com.linchomspace.homepage;
+package linchom.com.linchomspace.homepage.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import linchom.com.linchomspace.R;
 
-public class NavigationActivity extends AppCompatActivity {
+public class CommentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
+        setContentView(R.layout.activity_comment);
     }
 }
--- app/src/main/java/linchom/com/linchomspace/homepage/Activity/NavigationActivity.java
+++ app/src/main/java/linchom/com/linchomspace/homepage/Activity/NavigationActivity.java
@@ -1,15 +1,15 @@
-package linchom.com.linchomspace.homepage;
+package linchom.com.linchomspace.homepage.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import linchom.com.linchomspace.R;
 
-public class VideoActivity extends AppCompatActivity {
+public class NavigationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video);
+        setContentView(R.layout.activity_navigation);
     }
 }
