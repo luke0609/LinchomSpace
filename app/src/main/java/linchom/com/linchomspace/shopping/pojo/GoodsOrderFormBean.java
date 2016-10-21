32915aa5cfc96904f5e55015940a0749e0619b98
luke0609 <ljs19950609@163.com>
2016-10-21 10:04:55 +0800

    author:徐小龙 desc:10/21修改

---
+++ app/src/main/java/linchom/com/linchomspace/shopping/pojo/GoodsOrderFormBean.java
@@ -0,0 +1,72 @@
+package linchom.com.linchomspace.shopping.pojo;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/10/21.
+ */
+
+public class GoodsOrderFormBean {
+
+
+    public String result;
+
+    public Data data;
+
+    public String msg;
+
+    public String url;
+
+
+    public class  Data{
+
+        public String total_pages;
+
+        public  String total;
+
+
+        public List<OrderForm> items;
+
+        public String pagesize;
+
+
+    }
+
+
+
+
+    public class OrderForm{
+
+
+            public String order_id;
+            public String order_sn;
+            public String user_id;
+
+            public String order_status;
+            public String shipping_status;
+            public String pay_status;
+            public String confirm_time;
+        public List<OrderInfo> order_goods;
+
+
+    }
+
+    public class OrderInfo{
+        public String    goods_name;
+        public String    order_id;
+        public String    goods_id;
+        public String    goods_sn;
+        public String    goods_number;
+        public String    goods_price;
+        public String    total;
+        public String    goodsnum;
+        public String    goods_img;
+
+
+    }
+
+
+
+
+
+}
