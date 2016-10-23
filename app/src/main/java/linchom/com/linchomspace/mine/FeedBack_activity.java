cee43a7ce06fe1a2ebdf8158c925a9be696634b4
lenovo <ljs19950609@163.com>
2016-10-23 20:11:38 +0800

    author:LcL desc:10/22

--- app/src/main/java/linchom/com/linchomspace/mine/FeedBack_activity.java
+++ app/src/main/java/linchom/com/linchomspace/mine/FeedBack_activity.java
@@ -4,8 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 import linchom.com.linchomspace.R;
 
@@ -13,15 +20,49 @@ public class FeedBack_activity extends AppCompatActivity {
 
     private ImageView iv_fkback;
     private Button b_ok;
+    private EditText et_dis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_back_activity);
 
+        et_dis = ((EditText) findViewById(R.id.et_dis));
+
         b_ok = ((Button) findViewById(R.id.b_ok));
         b_ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                final String msg=et_dis.getText().toString();
+
+                Gson gson=new Gson();
+
+                RequestParams requestParams=new RequestParams("http://app.linchom.com/appapi.php?act=user_add_customer&user_id=135&content=321321");
+                requestParams.addQueryStringParameter("msg",msg);
+                x.http().post(requestParams, new Callback.CommonCallback<String>() {
+
+                    @Override
+                    public void onSuccess(String result) {
+                        System.out.println("onsucess"+result);
+                    }
+
+                    @Override
+                    public void onError(Throwable ex, boolean isOnCallback) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(CancelledException cex) {
+
+                    }
+
+                    @Override
+                    public void onFinished() {
+
+                    }
+                });
+
                 Toast.makeText(getApplicationContext(),"反馈提交成功",Toast.LENGTH_SHORT).show();
                 finish();
             }
