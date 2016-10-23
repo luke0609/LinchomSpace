900b050c69feab6e8b24a96873bf1ec68a11f83f
luke0609 <ljs19950609@163.com>
2016-10-23 13:59:15 +0800

    author:徐小龙 desc:10/23修改

--- app/src/main/java/linchom/com/linchomspace/shopping/GoodsAreaActivity.java
+++ app/src/main/java/linchom/com/linchomspace/shopping/GoodsAreaActivity.java
@@ -1,15 +1,54 @@
 package linchom.com.linchomspace.shopping;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import linchom.com.linchomspace.R;
 
 public class GoodsAreaActivity extends AppCompatActivity {
 
+    private Button btn_goods_area_addaddress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_area);
+
+        initView();
+
+
+        initData();
+
+        initEvent();
+    }
+
+    private void initView() {
+        btn_goods_area_addaddress = ((Button) findViewById(R.id.btn_goods_area_addaddress));
+
+
+    }
+
+    private void initData() {
+
+
+    }
+
+    private void initEvent() {
+        btn_goods_area_addaddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(GoodsAreaActivity.this,AddaddressActivity.class);
+
+                startActivity(intent);
+
+
+
+            }
+        });
+
+
     }
 }
