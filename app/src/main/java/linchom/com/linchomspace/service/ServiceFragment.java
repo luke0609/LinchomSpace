a39a4ab05759ad8aa3c906699e1530f5e3514c33
luke0609 <ljs19950609@163.com>
2016-10-25 14:06:30 +0800

    123

--- app/src/main/java/linchom/com/linchomspace/service/ServiceFragment.java
+++ app/src/main/java/linchom/com/linchomspace/service/ServiceFragment.java
@@ -7,11 +7,17 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import linchom.com.linchomspace.R;
+
+import static linchom.com.linchomspace.R.drawable.add;
 
 public class ServiceFragment extends Fragment {
 
@@ -19,6 +25,10 @@ public class ServiceFragment extends Fragment {
     private ViewPager vp_service;
     private List<View> viewList = new ArrayList<View>();
     private LayoutInflater inflater;
+    private RadioGroup rg_choice;
+    private RadioButton request;
+    private RadioButton service;
+    private PullToRefreshListView plv_1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,16 +40,18 @@ public class ServiceFragment extends Fragment {
     }
 
     private void initView() {
-
-
+        request = ((RadioButton) view_main.findViewById(R.id.request));
+        service = ((RadioButton) view_main.findViewById(R.id.serviec));
+        rg_choice = ((RadioGroup) view_main.findViewById(R.id.rg_choice));
         vp_service = ((ViewPager) view_main.findViewById(R.id.vp_service));
+
         inflater = LayoutInflater.from(getContext());
-               //获取四个view
+
                View view1 = inflater.inflate(R.layout.service_require_layout, null);
                View view2 = inflater.inflate(R.layout.service_service_layout, null);
             viewList.add(view1);
             viewList.add(view2);
-
+        plv_1 = ((PullToRefreshListView) view1.findViewById(R.id.lv));
         vp_service.setAdapter(new PagerAdapter() {
             @Override
             public int getCount() {
@@ -64,6 +76,24 @@ public class ServiceFragment extends Fragment {
 
             }
         });
+        vp_service.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) request.setChecked(true);
+               else service.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
 
 
