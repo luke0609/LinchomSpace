9285d2361fc0a705b1fde4f7e12a4b81cd9b4853
luke0609 <ljs19950609@163.com>
2016-10-25 14:45:49 +0800

    author:徐小龙 desc:修改

---
+++ app/src/main/java/linchom/com/linchomspace/shopping/OrderDetailActivity.java
@@ -0,0 +1,15 @@
+package linchom.com.linchomspace.shopping;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import linchom.com.linchomspace.R;
+
+public class OrderDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail);
+    }
+}
