7534de78b668235d518a2af5c50c5676bcb31d50
luke0609 <ljs19950609@163.com>
2016-10-26 20:08:57 +0800

    author:徐小龙 desc:修改

--- app/src/main/java/linchom/com/linchomspace/shopping/GoodsCartActivity.java
+++ app/src/main/java/linchom/com/linchomspace/shopping/GoodsCartActivity.java
@@ -3,13 +3,15 @@ package linchom.com.linchomspace.shopping;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
@@ -20,7 +22,9 @@ import org.xutils.http.RequestParams;
 import org.xutils.x;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import linchom.com.linchomspace.R;
 import linchom.com.linchomspace.shopping.contant.GoodsHttpUtils;
@@ -42,6 +46,20 @@ public class GoodsCartActivity extends AppCompatActivity {
     private List<GoodsCartBean.Data> cartList =new ArrayList<GoodsCartBean.Data>();
 
 
+    private Map<Integer,Boolean> checkStatusMap = new HashMap<Integer,Boolean>();
+
+    private Map<Integer,Boolean> checkModifyStatusMap = new HashMap<Integer,Boolean>();
+
+
+    private Map<Integer,String> buyNumMap = new HashMap<Integer,String>();
+
+
+    private Double totalPrice =0.0;
+    private TextView tv_goods_cart_totalprice;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +77,8 @@ public class GoodsCartActivity extends AppCompatActivity {
 
     private void initView() {
         ptr_cartList_ptr = ((PullToRefreshListView) findViewById(R.id.ptr_cartList_ptr));
+
+        tv_goods_cart_totalprice = ((TextView) findViewById(R.id.tv_goods_cart_totalprice));
 
 
     }
@@ -120,47 +140,139 @@ public class GoodsCartActivity extends AppCompatActivity {
             int buyNum;
 
             @Override
-            public void convert(GoodsViewHolder viewHolder, GoodsCartBean.Data data, int position) {
+            public void convert(GoodsViewHolder viewHolder, GoodsCartBean.Data data, final int position) {
+
+            final  CheckBox cb_goods_cart_choice=  viewHolder.getViewById(R.id.cb_goods_cart_choice);
 
             TextView iv_goods_cart_name =viewHolder.getViewById(R.id.iv_goods_cart_name);
             TextView tv_goods_cart_price=  viewHolder.getViewById(R.id.tv_goods_cart_price);
            final  TextView tv_goods_cart_num=   viewHolder.getViewById(R.id.tv_goods_cart_num);
 
-               RelativeLayout rl_goods_cart_sub= viewHolder.getViewById(R.id.rl_goods_cart_sub);
+                final  RelativeLayout rl_goods_cart_sub= viewHolder.getViewById(R.id.rl_goods_cart_sub);
 
-               final EditText et_goods_cart_buynum=  viewHolder.getViewById(R.id.et_goods_cart_buynum);
+           final TextView et_goods_cart_buynum=  viewHolder.getViewById(R.id.et_goods_cart_buynum);
 
-               RelativeLayout rl_goods_cart_add= viewHolder.getViewById(R.id.rl_goods_cart_add);
+                final  RelativeLayout rl_goods_cart_add= viewHolder.getViewById(R.id.rl_goods_cart_add);
 
 
             final CheckBox cb_goods_cart_modify= viewHolder.getViewById(R.id.cb_goods_cart_modify);
            final  RelativeLayout rl_goods_cart_modify= viewHolder.getViewById(R.id.rl_goods_cart_modify);
+
+            Button btn_goods_cart_delete=   viewHolder.getViewById(R.id.btn_goods_cart_delete);
+
+                btn_goods_cart_delete.setTag(position);
+
+
+                cb_goods_cart_choice.setTag(position);
+                cb_goods_cart_choice.setChecked(checkStatusMap.get(position));
+
+                cb_goods_cart_modify.setTag(position);
+                cb_goods_cart_modify.setChecked(checkModifyStatusMap.get(position));
+
+
+
+                rl_goods_cart_sub.setTag(position);
+                rl_goods_cart_add.setTag(position);
+
+
+                et_goods_cart_buynum.setTag(position);
+                et_goods_cart_buynum.setText(buyNumMap.get(position));
+
+
+                tv_goods_cart_num.setText(buyNumMap.get(position));
+
+                if(checkModifyStatusMap.get(position)){
+
+                    cb_goods_cart_modify.setText("完成");
+                    rl_goods_cart_modify.setVisibility(View.VISIBLE);
+
+                }else{
+
+                    cb_goods_cart_modify.setText("编辑");
+                    rl_goods_cart_modify.setVisibility(View.GONE);
+
+
+
+                }
+
+
 
 
                 iv_goods_cart_name.setText(data.goods_name);
 
                 tv_goods_cart_price.setText(data.goods_price);
 
-                 tv_goods_cart_num.setText(data.goods_number);
 
-                et_goods_cart_buynum.setText(data.goods_number);
+
+
+
+                cb_goods_cart_choice.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        if(isChecked&&position==(int)cb_goods_cart_choice.getTag()){
+
+                            checkStatusMap.put((int)buttonView.getTag(),true);
+
+                           double onePrice = Double.parseDouble(cartList.get((int)cb_goods_cart_choice.getTag()).goods_price);
+
+                           int oneNum= Integer.parseInt(buyNumMap.get((int)cb_goods_cart_choice.getTag()));
+
+                            totalPrice+= (onePrice*oneNum);
+
+                            tv_goods_cart_totalprice.setText("合计:"+totalPrice+"元");
+
+
+
+
+                        }else if(!isChecked&&position==(int)cb_goods_cart_choice.getTag()){
+
+
+
+                                checkStatusMap.put((int) buttonView.getTag(), false);
+
+
+                                double onePrice = Double.parseDouble(cartList.get((int) cb_goods_cart_choice.getTag()).goods_price);
+
+                                int oneNum = Integer.parseInt(buyNumMap.get((int) cb_goods_cart_choice.getTag()));
+
+                                totalPrice -= (onePrice * oneNum);
+
+                                tv_goods_cart_totalprice.setText("合计:" + totalPrice + "元");
+
+
+
+
+
+                        }
+
+                    }
+                });
 
 
 
                 cb_goods_cart_modify.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if(isChecked){
+                        if(isChecked&&position==(int)cb_goods_cart_modify.getTag()){
                             rl_goods_cart_modify.setVisibility(View.VISIBLE);
 
                             cb_goods_cart_modify.setText("完成");
 
+                            checkModifyStatusMap.put((int)buttonView.getTag(),true);
 
-                        }else{
+
+
+
+                        }else if(!isChecked&&position==(int)cb_goods_cart_modify.getTag()){
 
                             rl_goods_cart_modify.setVisibility(View.GONE);
                             cb_goods_cart_modify.setText("编辑");
-                            tv_goods_cart_num.setText(buyNum+"");
+                            tv_goods_cart_num.setText(buyNumMap.get((int)cb_goods_cart_modify.getTag()));
+                            checkModifyStatusMap.put((int)buttonView.getTag(),false);
+
+                            Log.i(TAG,"checkModifyStatusMap"+checkModifyStatusMap);
+
 
                         }
 
@@ -173,19 +285,32 @@ public class GoodsCartActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
 
-                        buyNum =Integer.parseInt(et_goods_cart_buynum.getText().toString());
+                        if(position==(int)et_goods_cart_buynum.getTag()&&position==(int)rl_goods_cart_sub.getTag()) {
 
-                        if(buyNum>1){
-                            buyNum--;
+                            buyNum = Integer.parseInt(et_goods_cart_buynum.getText().toString());
+
+                            if (buyNum > 1) {
+                                buyNum--;
+
+                                if (checkStatusMap.get((int) et_goods_cart_buynum.getTag())) {
+
+                                    double onePrice = Double.parseDouble(cartList.get((int) cb_goods_cart_choice.getTag()).goods_price);
+
+                                    totalPrice -= onePrice;
+
+                                    tv_goods_cart_totalprice.setText("合计:" + totalPrice + "元");
+
+
+                                }
+
+
+                            }
+                            buyNumMap.put((int)v.getTag(),buyNum+"");
+
+                            et_goods_cart_buynum.setText(buyNum + "");
 
 
                         }
-
-
-
-
-
-                        et_goods_cart_buynum.setText(buyNum+"");
 
 
                     }
@@ -195,17 +320,95 @@ public class GoodsCartActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
 
+                        if(position==(int)et_goods_cart_buynum.getTag()&&position==(int)rl_goods_cart_add.getTag()) {
 
-                        buyNum =Integer.parseInt(et_goods_cart_buynum.getText().toString());
 
-                        if(buyNum<10){
-                            buyNum++;
+                            buyNum = Integer.parseInt(et_goods_cart_buynum.getText().toString());
 
+
+                                buyNum++;
+
+
+
+                            buyNumMap.put((int)v.getTag(),buyNum+"");
+
+                            Log.i(TAG,"buyNumMap"+buyNumMap);
+
+
+
+                            et_goods_cart_buynum.setText(buyNum + "");
+
+
+                            if(checkStatusMap.get((int)et_goods_cart_buynum.getTag())){
+
+                                double onePrice = Double.parseDouble(cartList.get((int)cb_goods_cart_choice.getTag()).goods_price);
+
+                                totalPrice+=onePrice;
+
+                                tv_goods_cart_totalprice.setText("合计:"+totalPrice+"元");
+
+
+                            }
 
                         }
 
+                    }
+                });
 
-                        et_goods_cart_buynum.setText(buyNum+"");
+
+                btn_goods_cart_delete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        RequestParams requestParams =new RequestParams(GoodsHttpUtils.SHOPURL);
+
+                        requestParams.addBodyParameter("act","flow_drop_cart_goods");
+
+                        requestParams.addBodyParameter("rec_id",cartList.get((int)v.getTag()).rec_id+"");
+
+
+                        requestParams.addBodyParameter("user_id",userId+"");
+
+                        x.http().post(requestParams, new Callback.CommonCallback<String>() {
+                            @Override
+                            public void onSuccess(String result) {
+
+
+                              //  Log.i(TAG,"result"+result);
+                               // Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+
+
+                                Toast.makeText(getApplicationContext(),"删除成功",Toast.LENGTH_SHORT).show();
+
+
+
+
+                                getData();
+                            }
+
+                            @Override
+                            public void onError(Throwable ex, boolean isOnCallback) {
+                                Toast.makeText(getApplicationContext(),"删除失败",Toast.LENGTH_SHORT).show();
+
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(CancelledException cex) {
+
+                            }
+
+                            @Override
+                            public void onFinished() {
+
+                            }
+                        });
+
+
+
+
 
                     }
                 });
@@ -222,11 +425,12 @@ public class GoodsCartActivity extends AppCompatActivity {
     }
 
     private void getData() {
+
         RequestParams requestParams =new RequestParams(GoodsHttpUtils.SHOPURL);
 
         requestParams.addBodyParameter("act","cartgoods");
 
-        requestParams.addBodyParameter("user_id","12");
+        requestParams.addBodyParameter("user_id",userId+"");
         x.http().post(requestParams, new Callback.CommonCallback<String>() {
             @Override
             public void onSuccess(String result) {
@@ -235,7 +439,40 @@ public class GoodsCartActivity extends AppCompatActivity {
 
                 Gson gson =new Gson();
                 GoodsCartBean goodsCartBean =  gson.fromJson(result,GoodsCartBean.class);
+
+                cartList.clear();
                 cartList.addAll(goodsCartBean.data);
+
+                Log.i(TAG,cartList.size()+"");
+
+
+                for(int i=0;i<cartList.size();i++){
+
+
+                    checkStatusMap.put(i,false);
+
+
+                }
+
+                for(int i=0;i<cartList.size();i++){
+
+
+                    checkModifyStatusMap.put(i,false);
+
+
+                }
+
+
+                for(int i=0;i<cartList.size();i++){
+
+
+
+                    buyNumMap.put(i,cartList.get(i).goods_number);
+                }
+
+                //totalPrice=0.0;
+
+               // tv_goods_cart_totalprice.setText("合计:0.0元");
 
                 goodsCommonAdapter.notifyDataSetChanged();
 
@@ -244,6 +481,13 @@ public class GoodsCartActivity extends AppCompatActivity {
 
             @Override
             public void onError(Throwable ex, boolean isOnCallback) {
+                //totalPrice=0.0;
+
+               // tv_goods_cart_totalprice.setText("合计:0.0元");
+
+
+                cartList.clear();
+                goodsCommonAdapter.notifyDataSetChanged();
 
 
 
