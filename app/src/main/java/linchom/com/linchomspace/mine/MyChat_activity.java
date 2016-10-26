8d23db58d270c9a80fa03344e6595f66a08867d1
lenovo <ljs19950609@163.com>
2016-10-26 19:50:08 +0800

    author:LcL desc:10/26

--- app/src/main/java/linchom/com/linchomspace/mine/MyChat_activity.java
+++ app/src/main/java/linchom/com/linchomspace/mine/MyChat_activity.java
@@ -1,10 +1,13 @@
 package linchom.com.linchomspace.mine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +27,7 @@ import java.util.List;
 import java.util.Locale;
 
 import linchom.com.linchomspace.R;
+import linchom.com.linchomspace.chat.ChatDetilActivity;
 import linchom.com.linchomspace.mine.pojo.LovedInfoBean;
 import linchom.com.linchomspace.mine.pojo.MychatInfoBean;
 import linchom.com.linchomspace.shopping.goodsadapter.GoodsCommonAdapter;
@@ -38,6 +42,7 @@ public class MyChat_activity extends AppCompatActivity {
     private PullToRefreshListView lv_myChatList;
     private int page=1;
     private int pageCount=1;
+    private ProgressBar firstBar;
 
 
 //    private ListView lv_myChatList;
@@ -47,12 +52,29 @@ public class MyChat_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_chat_activity);
         lv_myChatList = ((PullToRefreshListView) findViewById(R.id.lv_myChatList));
+        firstBar = ((ProgressBar) findViewById(R.id.firstBar));
 //        lv_myChatList = ((ListView) findViewById(R.id.lv_myChatList));
 
 //        System.out.println("onCreate");
         initView();
         initData();
         eventPullToRefresh();
+        lv_myChatList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MychatInfoBean.Mdata.Imtems topic=chatlist.get(position-1);
+                Intent intent = new Intent(getApplicationContext(), ChatDetilActivity.class);
+                intent.putExtra("topic_id",topic.getTopic_id());
+                startActivity(intent);
+            }
+        });
+
+        iv_chat_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     private void eventPullToRefresh() {
@@ -64,10 +86,37 @@ public class MyChat_activity extends AppCompatActivity {
 
                 page++;
                 initData();
-                lv_myChatList.onRefreshComplete();
+                lv_myChatList.postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        lv_myChatList.onRefreshComplete();
+                    }
+                }, 1000);
 
             }
         });
+
+        chatCommonAdapter=new GoodsCommonAdapter<MychatInfoBean.Mdata.Imtems>(getApplicationContext(), chatlist, R.layout.my_chat_items) {
+            @Override
+            public void convert(GoodsViewHolder viewHolder, MychatInfoBean.Mdata.Imtems imtems, int position) {
+                TextView topic_name = (viewHolder.getViewById(R.id.topic_name));
+                System.out.println("=========" + imtems.topic_name);
+                topic_name.setText(imtems.topic_name);
+                TextView communication_title = (viewHolder.getViewById(R.id.communication_title));
+                communication_title.setText(imtems.communication_title);
+                TextView user_name = viewHolder.getViewById(R.id.user_name);
+                user_name.setText(imtems.user_name);
+                TextView add_time = viewHolder.getViewById(R.id.add_time);
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日", Locale.getDefault());
+                Date date = new Date(Long.parseLong(imtems.add_time));
+                add_time.setText(sdf.format(date));
+            }
+        };
+
+        lv_myChatList.setAdapter(chatCommonAdapter);
+
+        initData();
 
 
     }
@@ -105,55 +154,40 @@ public class MyChat_activity extends AppCompatActivity {
 
     private void initData() {
 //        System.out.println("initData");
+        if (page==1) {
+            firstBar.setVisibility(View.VISIBLE);
+        }
         RequestParams requestParams=new RequestParams("http://app.linchom.com/appapi.php?act=topic&user_id=135");
         requestParams.addBodyParameter("verification","e0d017ef76c8510244ebe0191f5dde15" );
         requestParams.addBodyParameter("page",page+"");
 
         x.http().post(requestParams, new Callback.CommonCallback<String>() {
+
             @Override
             public void onSuccess(String result) {
-//                System.out.println("onSuccess");
-//                System.out.println("onSuccess"+result);
+
+                System.out.println("========="+page+"0000000000"+pageCount);
                 if (page <= pageCount) {
                     Gson gson = new Gson();
                     MychatInfoBean mychatInfoBean = gson.fromJson(result, MychatInfoBean.class);
-//                System.out.println(mychatInfoBean);
                     pageCount = Integer.parseInt(mychatInfoBean.data.total_pages.toString());
                     MychatInfoBean.Mdata mdata = mychatInfoBean.getData();
-//                System.out.println(mdata);
                     chatlist.addAll(mdata.items);
-                    //页面添加数据
-//                    initEvent();
 
-                    //  MychatInfoBean.Mdata mdata=gson.fromJson(result,MychatInfoBean.Mdata.class);
+                    chatCommonAdapter.notifyDataSetChanged();
 
-                    chatCommonAdapter = new GoodsCommonAdapter<MychatInfoBean.Mdata.Imtems>(getApplicationContext(), chatlist, R.layout.my_chat_items) {
-                        @Override
-                        public void convert(GoodsViewHolder viewHolder, MychatInfoBean.Mdata.Imtems imtems, int position) {
-                            TextView topic_name = (viewHolder.getViewById(R.id.topic_name));
-                            System.out.println("=========" + imtems.topic_name);
-                            topic_name.setText(imtems.topic_name);
-                            TextView communication_title = (viewHolder.getViewById(R.id.communication_title));
-                            communication_title.setText(imtems.communication_title);
-                            TextView user_name = viewHolder.getViewById(R.id.user_name);
-                            user_name.setText(imtems.user_name);
-                            TextView add_time = viewHolder.getViewById(R.id.add_time);
-                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日", Locale.getDefault());
-                            Date date=new Date(Long.parseLong(imtems.add_time));
-                            add_time.setText(sdf.format(date));
-                        }
-                    };
-                    lv_myChatList.setAdapter(chatCommonAdapter);
-                } else {
-
-                    Toast.makeText(getApplicationContext(), "已经是最后一页了", Toast.LENGTH_SHORT).show();
-
+                    System.out.println("=====" + chatlist);
+                }else {
+                    Toast.makeText(getApplicationContext(),"已经是最后一页了",Toast.LENGTH_SHORT).show();
+                    chatCommonAdapter.notifyDataSetChanged();
                 }
+
+                firstBar.setVisibility(View.GONE);
             }
+
             @Override
             public void onError(Throwable ex, boolean isOnCallback) {
 
-                System.out.println("onError"+ex+"");
             }
 
             @Override
@@ -164,13 +198,6 @@ public class MyChat_activity extends AppCompatActivity {
             @Override
             public void onFinished() {
 
-            }
-        });
-
-        iv_chat_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
             }
         });
     }
