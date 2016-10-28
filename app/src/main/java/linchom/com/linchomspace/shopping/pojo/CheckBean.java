bff7e0ff12dcb22cfb971674bfa908c6e3a1bbdd
luke0609 <ljs19950609@163.com>
2016-10-28 14:28:08 +0800

    author:徐小龙 desc:修改

---
+++ app/src/main/java/linchom/com/linchomspace/shopping/pojo/CheckBean.java
@@ -0,0 +1,19 @@
+package linchom.com.linchomspace.shopping.pojo;
+
+import android.widget.CheckBox;
+
+/**
+ * Created by Administrator on 2016/10/28.
+ */
+
+public class CheckBean {
+
+    public int position;
+
+    public CheckBox checkBox;
+
+    public CheckBean(int position, CheckBox checkBox) {
+        this.position = position;
+        this.checkBox = checkBox;
+    }
+}
