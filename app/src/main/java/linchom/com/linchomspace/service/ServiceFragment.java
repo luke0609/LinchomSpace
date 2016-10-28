684d7067f1d2af4b7524485adbc1852211c08761
luke0609 <ljs19950609@163.com>
2016-10-28 15:40:49 +0800

    123

--- app/src/main/java/linchom/com/linchomspace/service/ServiceFragment.java
+++ app/src/main/java/linchom/com/linchomspace/service/ServiceFragment.java
@@ -1,5 +1,6 @@
 package linchom.com.linchomspace.service;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
@@ -9,10 +10,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,7 +38,9 @@ import linchom.com.linchomspace.service.utils.ResUtil;
 import linchom.com.linchomspace.service.utils.WheelDialogFragment;
 
 import static linchom.com.linchomspace.R.drawable.add;
+import static linchom.com.linchomspace.R.drawable.cascades;
 import static linchom.com.linchomspace.R.id.lv;
+import static linchom.com.linchomspace.R.id.plus_im;
 import static linchom.com.linchomspace.R.id.service_category;
 import static linchom.com.linchomspace.R.layout.cell;
 
@@ -55,16 +61,44 @@ public class ServiceFragment extends Fragment implements View.OnClickListener {
     private ImageView address_select;
     private TextView tv_address;
 
+    private boolean clicked = false;// 记录加号按钮的点击状态，默认为没有点击
+
+    private RelativeLayout plus_rl;
+    private ImageView plus_im;
+    private TextView dishui_tv, guoshui_tv;
+
+    private Animation rotate_anticlockwise, rotate_clockwise, scale_max,
+            scale_min, alpha_button;
+    private RelativeLayout rl_plus;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         view_main = inflater.inflate(R.layout.fragment_service, container, false);
+        initData();
         initView();
         return view_main;
     }
+    private void initData() {
+        // TODO Auto-generated method stub
+        rotate_anticlockwise = AnimationUtils.loadAnimation(getContext(),
+                R.anim.rotate_anticlockwise);
+        rotate_clockwise = AnimationUtils.loadAnimation(getContext(),
+                R.anim.rotate_clockwise);
+        scale_max = AnimationUtils.loadAnimation(getContext(), R.anim.scale_max);
+        scale_min = AnimationUtils.loadAnimation(getContext(), R.anim.scale_min);
+        alpha_button = AnimationUtils.loadAnimation(getContext(), R.anim.alpha_button);
+    }
 
     private void initView() {
+        plus_rl = (RelativeLayout) view_main.findViewById(R.id.plus_rl);
+        rl_plus = ((RelativeLayout) view_main.findViewById(R.id.rl_plus));
+        plus_im = (ImageView) view_main.findViewById(R.id.plus_im);
+        dishui_tv = (TextView) view_main.findViewById(R.id.dishui_tv);
+        guoshui_tv = (TextView)view_main.findViewById(R.id.guoshui_tv);
+
+
         service_category = ((TextView) view_main.findViewById(R.id.service_category));
         request = ((RadioButton) view_main.findViewById(R.id.request));
         service = ((RadioButton) view_main.findViewById(R.id.serviec));
@@ -72,9 +106,14 @@ public class ServiceFragment extends Fragment implements View.OnClickListener {
         vp_service = ((ViewPager) view_main.findViewById(R.id.vp_service));
         address_select = ((ImageView) view_main.findViewById(R.id.address_select));
         tv_address = ((TextView) view_main.findViewById(R.id.tv_address));
+
         address_select.setOnClickListener(this);
         service_category.setOnClickListener(this);
         tv_address.setOnClickListener(this);
+        rl_plus.setOnClickListener(this);
+        dishui_tv.setOnClickListener(this);
+        guoshui_tv.setOnClickListener(this);
+        dishui_tv.setOnClickListener(this);
         inflater = LayoutInflater.from(getContext());
 
         View view1 = inflater.inflate(R.layout.service_require_layout, null);
@@ -187,7 +226,18 @@ public class ServiceFragment extends Fragment implements View.OnClickListener {
             case R.id.tv_address:
                 up_choice_address();
                 break;
-        }
+            case R.id.rl_plus:
+                show_round();
+                break;
+            case R.id.dishui_tv:
+                v.startAnimation(alpha_button);
+                rl_plus.performClick();
+                break;
+            case R.id.guoshui_tv:
+                v.startAnimation(alpha_button);
+                rl_plus.performClick();
+                break;
+            }
 
 
     }
@@ -242,4 +292,28 @@ public class ServiceFragment extends Fragment implements View.OnClickListener {
         });
         wheelViewDialogFragment.show(getFragmentManager(), "");
     }
+    public void show_round(){
+        // TODO Auto-generated method stub
+        clicked = !clicked;
+        // 两个按钮的显示隐藏
+        dishui_tv.setVisibility(clicked ? View.VISIBLE : View.GONE);
+        guoshui_tv.setVisibility(clicked ? View.VISIBLE : View.GONE);
+        // 加号旋转
+        plus_im.startAnimation(clicked ? rotate_anticlockwise
+                : rotate_clockwise);
+        // 按钮显示隐藏效果
+        dishui_tv.startAnimation(clicked ? scale_max : scale_min);
+        guoshui_tv.startAnimation(clicked ? scale_max : scale_min);
+        // 背景色的改变
+        plus_rl.setBackgroundColor(clicked ? Color
+                .parseColor("#ddffffff") : Color.TRANSPARENT);
+        // 背景是否可点击，用于控制Framelayout层下面的视图是否可点击
+        plus_rl.setClickable(clicked);
+        dishui_tv.setClickable(clicked ?true :false);
+        guoshui_tv.setClickable(clicked ?true :false);
+    }
+    public void publish_service(){
+
+    }
+    public void mine_service(){}
 }
