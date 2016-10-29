71155a876b104cac6c82758fec4532fb28945a37
luke0609 <ljs19950609@163.com>
2016-10-29 09:46:12 +0800

    author:徐小龙 desc:修改

---
+++ app/src/main/java/linchom/com/linchomspace/shopping/pojo/OrderSuccessBean.java
@@ -0,0 +1,85 @@
+package linchom.com.linchomspace.shopping.pojo;
+
+/**
+ * Created by Administrator on 2016/10/29.
+ */
+
+public class OrderSuccessBean {
+
+
+    public String result;
+
+    public Data data;
+
+    public String msg;
+
+    public String url;
+
+
+    public class Data{
+
+
+
+                public String shipping_id;
+                public String pay_id;
+                public String pack_id;
+                public String card_id;
+                public String card_message;
+                public String surplus;
+                public String integral;
+                public String bonus_id;
+                public String need_inv;
+                public String inv_type;
+                public String inv_payee;
+                public String inv_content;
+                public String postscript;
+                public String how_oos;
+                public String need_insure;
+                public String user_id;
+                public String add_time;
+                public String order_status;
+                public String shipping_status;
+                public String pay_status;
+                public String agency_id;
+                public String extension_code;
+                public String extension_id;
+                public String pay_name;
+                public String referer;
+                public String mobile_pay;
+                public String mobile_order;
+                public String address_id;
+                public String address_name;
+                public String consignee;
+                public String email;
+                public String country;
+                public String province;
+                public String city;
+                public String district;
+                public String address;
+                public String zipcode;
+                public String tel;
+                public String mobile;
+                public String sign_building;
+                public String best_time;
+                public String bonus;
+                public String goods_amount;
+                public String discount;
+                public String tax;
+                public String order_sn;
+                public String order_id;
+                public String log_id;
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
