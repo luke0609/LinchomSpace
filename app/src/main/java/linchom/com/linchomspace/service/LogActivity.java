c8ee10e0e84931a014e5f965cdb8c709d8ddd8e4
luke0609 <ljs19950609@163.com>
2016-11-09 18:12:02 +0800

    123

--- app/src/main/java/linchom/com/linchomspace/service/LogActivity.java
+++ app/src/main/java/linchom/com/linchomspace/service/LogActivity.java
@@ -53,115 +53,115 @@ public class LogActivity extends AppCompatActivity {
         recyclerView.init(this,MultiPickResultView.ACTION_SELECT,null);
     }
 
-    @OnClick({R.id.iv_back, R.id.btn_publish})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.iv_back:
-                finish();
-                break;
-            case R.id.btn_publish:
-                if (etTitle.getText().toString().length() == 0
-                        || addContent.getText().toString().length() == 0)
-                         {
-                            Toast.makeText(getApplicationContext(), "请完善信息", Toast.LENGTH_SHORT).show();
-                             break;
-                             }
-                else {
-                    if(list_photo.isEmpty()){
-                        doPublish();
-                    }else {
-                        for(int i=0; i<list_photo.size();i++){
-                            photoUpload(i);
-                        }
+                @OnClick({R.id.iv_back, R.id.btn_publish})
+                public void onClick(View view) {
+                    switch (view.getId()) {
+                        case R.id.iv_back:
+                            finish();
+                            break;
+                        case R.id.btn_publish:
+                            if (etTitle.getText().toString().length() == 0
+                                    || addContent.getText().toString().length() == 0)
+                            {
+                                Toast.makeText(getApplicationContext(), "请完善信息", Toast.LENGTH_SHORT).show();
+                                break;
+                            }
+                            else {
+                                if(list_photo.isEmpty()){
+                                    doPublish();
+                                }else {
+                                    for(int i=0; i<list_photo.size();i++){
+                                        photoUpload(i);
+                                    }
+                                }
+                            }
                     }
                 }
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        recyclerView.onActivityResult(requestCode, resultCode, data);
-        list_photo=recyclerView.getPhotos();
-        //  recycler_view.showPics(list_photo);
+                @Override
+                protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+                    super.onActivityResult(requestCode, resultCode, data);
+                    recyclerView.onActivityResult(requestCode, resultCode, data);
+                    list_photo=recyclerView.getPhotos();
+                    //  recycler_view.showPics(list_photo);
 
-        for (int i=0;i<list_photo.size();i++){
-            System.out.println(list_photo.get(i));
-        }
+                    for (int i=0;i<list_photo.size();i++){
+                        System.out.println(list_photo.get(i));
+                    }
 
-    }
-    private void photoUpload(int i){
-        System.out.println(list_photo.get(i));
-
-        RequestParams params = new RequestParams("http://app.linchom.com/appapi.php");
-
-        params.addBodyParameter("act", "uploadimage");
-
-        params.addBodyParameter("goods_img",new File(list_photo.get(i)));
-        System.out.println(params);
-        x.http().post(params, new Callback.CommonCallback<String>() {
-
-
-            @Override
-            public void onSuccess(String result) {
-                System.out.println(result);
-                Gson gson = new Gson();
-                UploadBean bean = gson.fromJson(result, UploadBean.class);
-                photo=bean.getData();
-
-                photoAddress=photoAddress+photo+",";
-                count++;
-                System.out.println(photoAddress);
-                System.out.println(count);
-                if (count==list_photo.size()){
-                    System.out.println("xxxxxxx");
-                    doPublish();
                 }
+            private void photoUpload(int i){
+                System.out.println(list_photo.get(i));
+
+                RequestParams params = new RequestParams("http://app.linchom.com/appapi.php");
+
+                params.addBodyParameter("act", "uploadimage");
+
+                params.addBodyParameter("goods_img",new File(list_photo.get(i)));
+                System.out.println(params);
+                x.http().post(params, new Callback.CommonCallback<String>() {
+
+
+                    @Override
+                    public void onSuccess(String result) {
+                        System.out.println(result);
+                        Gson gson = new Gson();
+                        UploadBean bean = gson.fromJson(result, UploadBean.class);
+                        photo=bean.getData();
+
+                        photoAddress=photoAddress+photo+",";
+                        count++;
+                        System.out.println(photoAddress);
+                        System.out.println(count);
+                        if (count==list_photo.size()){
+                            System.out.println("xxxxxxx");
+                            doPublish();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable ex, boolean isOnCallback) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(CancelledException cex) {
+
+                    }
+
+                    @Override
+                    public void onFinished() {
+
+                    }
+                });
             }
+            private void doPublish() {
+                String title = etTitle.getText().toString().trim();
 
-            @Override
-            public void onError(Throwable ex, boolean isOnCallback) {
+                String content = addContent.getText().toString().trim();
+                if("".equals(title)){
+                    Toast.makeText(this, "标题不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if("".equals(content)){
+                    Toast.makeText(this, "评论内容不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                RequestParams params = new RequestParams("http://app.linchom.com/appapi.php");
+                params.addQueryStringParameter("act", "add_demand_services_log");
+                params.addQueryStringParameter("user_id", 135+ "");
+                params.addQueryStringParameter("title",title);
+                params.addQueryStringParameter("content",content);
+                params.addQueryStringParameter("photo",photoAddress);
 
-            }
-
-            @Override
-            public void onCancelled(CancelledException cex) {
-
-            }
-
-            @Override
-            public void onFinished() {
-
-            }
-        });
-    }
-    private void doPublish() {
-        String title = etTitle.getText().toString().trim();
-
-        String content = addContent.getText().toString().trim();
-        if("".equals(title)){
-            Toast.makeText(this, "标题不能为空", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if("".equals(content)){
-            Toast.makeText(this, "评论内容不能为空", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        RequestParams params = new RequestParams("http://app.linchom.com/appapi.php");
-        params.addQueryStringParameter("act", "add_demand_services_log");
-        params.addQueryStringParameter("user_id", 135+ "");
-        params.addQueryStringParameter("title",title);
-        params.addQueryStringParameter("content",content);
-        params.addQueryStringParameter("photo",photoAddress);
-
-        System.out.println(params);
-        x.http().get(params, new Callback.CommonCallback<String>() {
+                System.out.println(params);
+                x.http().get(params, new Callback.CommonCallback<String>() {
 
 
-            @Override
-            public void onSuccess(String result) {
-                System.out.println(result);
-                Toast.makeText(LogActivity.this, "发布成功", Toast.LENGTH_SHORT).show();
-                finish();
+                    @Override
+                    public void onSuccess(String result) {
+                        System.out.println(result);
+                        Toast.makeText(LogActivity.this, "发布成功", Toast.LENGTH_SHORT).show();
+                        finish();
             }
 
             @Override
