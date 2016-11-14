6b69b1c898ef1a9d9a5f5ed186b1068345ce1f1a
lenovo <ljs19950609@163.com>
2016-11-14 08:48:14 +0800

    author:lcl desc:11/9

--- app/src/main/java/linchom/com/linchomspace/mine/HeadImage_activity.java
+++ app/src/main/java/linchom/com/linchomspace/mine/HeadImage_activity.java
@@ -41,7 +41,7 @@ public class HeadImage_activity extends AppCompatActivity {
 
     public static final String TAG =" Details_Activity" ;
     private ImageView iv_back1;
-    private EditText tv_user_name;
+    private EditText tv_name;
     private TextView tv_sex;
     private TextView tv_birthday;
     private EditText tv_email;
@@ -77,7 +77,7 @@ public class HeadImage_activity extends AppCompatActivity {
         }
 
          iv_user_photo=((ImageView) findViewById(R.id.iv_user_photo));
-        tv_user_name = ((EditText)findViewById(R.id.tv_user_name));
+        tv_name = ((EditText)findViewById(R.id.tv_name));
         tv_sex = ((TextView) findViewById(R.id.tv_add));
         tv_birthday = ((TextView) findViewById(R.id.tv_birthday1));
         tv_email = ((EditText) findViewById(R.id.tv_email));
@@ -121,7 +121,7 @@ public class HeadImage_activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
-//                System.out.println("onCreatefinish");
+
             }
         });
 
@@ -132,7 +132,7 @@ public class HeadImage_activity extends AppCompatActivity {
                 Intent intent=new Intent(getApplicationContext(),Details_Activity.class);
                 Bundle bundle=new Bundle();
                 uploadImage();
-                System.out.println("dataBean"+dataBean);
+//                System.out.println("dataBean"+dataBean);
                 bundle.putSerializable("user",dataBean);
                 intent.putExtras(bundle);
                 startActivityForResult(intent,2);
@@ -178,7 +178,7 @@ public class HeadImage_activity extends AppCompatActivity {
         Intent intent=getIntent();
         dataBean= (UserInfoBean.DataBean) intent.getSerializableExtra("user");
         System.out.println("dataBean"+dataBean);
-        tv_user_name.setText(dataBean.getUser_name());
+        tv_name.setText(dataBean.getUser_name());
         tv_sex.setText(dataBean.getSex().equals("1")?"男":"女");
         tv_birthday.setText(dataBean.getBirthday());
         tv_email.setText(dataBean.getEmail());
@@ -215,21 +215,6 @@ public class HeadImage_activity extends AppCompatActivity {
                 }
                 break;
 
-//            case 2:
-//
-//                if (data != null) {
-//
-//                    Bitmap bitmap = data.getParcelableExtra("data");
-//
-//                    iv_image.setImageBitmap(bitmap);
-//
-//                } else {
-//                    System.out.println("000000"+ data);
-////                    Log.e(TAG, "CHOOSE_SMALL_PICTURE: data = " + data);
-//
-//                }
-//
-//                break;
         }
     }
 
@@ -314,7 +299,7 @@ public class HeadImage_activity extends AppCompatActivity {
 
     public void upData(String url){
 
-        String name=tv_user_name.getText().toString();
+        String name=tv_name.getText().toString();
         String sex=tv_sex.getText().toString();
         String birthday= tv_birthday.getText().toString();
         String[] birthdaies = birthday.split("-");
@@ -325,41 +310,29 @@ public class HeadImage_activity extends AppCompatActivity {
         dataBean=new UserInfoBean.DataBean(name,email,sex,birthday,officePhone,homePhone,mobilePhone);
 //        Gson gson=new Gson();
 ////      String userJson=gson.toJson(mUser);
-        RequestParams requestParams=new RequestParams("http://app.linchom.com/appapi.php?act=edituserinfo&user_id=135");
-        //requestParams.addBodyParameter("user_name","张晓文");
+        RequestParams requestParams=new RequestParams("http://app.linchom.com/appapi.php?act=edituserinfo");
+           requestParams.addBodyParameter("user_id","135");
         //requestParams.addQueryStringParameter("mUser",userJson);
         //requestParams.addQueryStringParameter("email",email);
         requestParams.addQueryStringParameter("user_name",name);
         requestParams.addQueryStringParameter("sex",sex.equals("男")?"1":"0");
-        requestParams.addQueryStringParameter("birthdayYear",birthdaies[0]);
-        requestParams.addQueryStringParameter("birthdayMonth",birthdaies[1]);
-        requestParams.addQueryStringParameter("birthdayDay",birthdaies[2]);
+//        requestParams.addQueryStringParameter("birthdayYear",birthdaies[0]);
+//        requestParams.addQueryStringParameter("birthdayMonth",birthdaies[1]);
+//        requestParams.addQueryStringParameter("birthdayDay",birthdaies[2]);
+        requestParams.addQueryStringParameter("birthday",birthday);
         requestParams.addQueryStringParameter("photo",photo);
 
         x.http().post(requestParams, new Callback.CommonCallback<String>() {
             @Override
             public void onSuccess(String result) {
 
-                System.out.println("6666666666666666"+result);
+//                System.out.println("6666666666666666"+result);
 
             }
             @Override
             public void onError(Throwable ex, boolean isOnCallback) {
                 System.out.println("error=="+ex+"");
-//                Log.e("error",ex.getMessage().toString());
-//                Toast.makeText(x.app(), ex.getMessage(), Toast.LENGTH_LONG).show();
-//                if (ex instanceof HttpException) { // 网络错误
-//                    HttpException httpEx = (HttpException) ex;
-//                    int responseCode = httpEx.getCode();
-//                    String responseMsg = httpEx.getMessage();
-//                    String errorResult = httpEx.getResult();
-//                    Log.e("网络错误",ex.getMessage().toString());
-//                    // ...
-//                } else { // 其他错误
-//
-//                    Log.e("其他错误",ex.getMessage().toString());
-//                    // ...
-//                }
+
             }
 
             @Override
@@ -391,14 +364,7 @@ public class HeadImage_activity extends AppCompatActivity {
             }
         });
         builder.show();
-//        new AlertDialog.Builder(this).setTitle("单选框").setIcon(
-//                android.R.drawable.ic_dialog_info).setSingleChoiceItems(
-//                new String[] { "Item1", "Item2" }, 0,
-//                new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int which) {
-////                        dialog.dismiss();
-//                    }
-//                }).setNegativeButton("取消", null).show();
+
 
     }
 
